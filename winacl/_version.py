9d509f46741c6d5775fd4c35afea11f44d8a4c21
SkelSec <info@skelsecprojects.com>
2022-09-16 00:02:03 +0200

    backupkey fun

--- winacl/_version.py
+++ winacl/_version.py
@@ -1,6 +1,6 @@
 
 
-__version__ = "0.1.4"
+__version__ = "0.1.5"
 __banner__ = \
 """
 # winacl %s 
--- winacl/dtyp/wcee/pvkfile.py
+++ winacl/dtyp/wcee/pvkfile.py
@@ -5,14 +5,27 @@ from winacl.dtyp.wcee.rsaprivkey import RSAPrivateKeyBlob
 # https://github.com/openssl/openssl/blob/9dddcd90a1350fa63486cbf3226c3eee79f9aff5/crypto/pem/pvkfmt.c
 class PVKFile:
 	def __init__(self):
-		self.magic = None
-		self.reserved = None
-		self.keytype = None
-		self.isencrypted = None
-		self.saltlength = None
-		self.keylength = None
-		self.saltblob = None
-		self.keyblob = None
+		self.magic:int = 0xb0b5f11e
+		self.reserved:int = 0
+		self.keytype:bytes = None
+		self.isencrypted:int = None
+		self.saltlength:int = None
+		self.keylength:int = None
+		self.saltblob:bytes = None
+		self.keyblob:bytes = None
+
+	@staticmethod
+	def construct_unencrypted(keytype, keydata):
+		if isinstance(keytype, str):
+			keytype = keytype.encode()
+		pvk = PVKFile()
+		pvk.keytype = keytype
+		pvk.isencrypted = 0
+		pvk.saltlength = 0
+		pvk.keylength = len(keydata)
+		pvk.saltblob = b''
+		pvk.keyblob = keydata
+		return pvk
 
 	@staticmethod
 	def from_file(filepath):
@@ -30,13 +43,29 @@ class PVKFile:
 		pvk.magic = int.from_bytes(buff.read(4), byteorder='little', signed=False)
 		assert 0xb0b5f11e == pvk.magic
 		pvk.reserved = int.from_bytes(buff.read(4), byteorder='little', signed=False)
-		pvk.keytype = int.from_bytes(buff.read(4), byteorder='little', signed=False)
+		pvk.keytype = buff.read(4)
 		pvk.isencrypted = int.from_bytes(buff.read(4), byteorder='little', signed=False)
 		pvk.saltlength = int.from_bytes(buff.read(4), byteorder='little', signed=False)
 		pvk.keylength = int.from_bytes(buff.read(4), byteorder='little', signed=False)
 		pvk.saltblob = buff.read(pvk.saltlength)
 		pvk.keyblob = buff.read(pvk.keylength)
 		return pvk
+
+	def to_bytes(self):
+		data = b''
+		data += self.magic.to_bytes(4, byteorder='little', signed=False)
+		data += self.reserved.to_bytes(4, byteorder='little', signed=False)
+		data += self.keytype
+		data += self.isencrypted.to_bytes(4, byteorder='little', signed=False)
+		if self.saltblob is None:
+			self.saltblob = b''
+		if self.keyblob is None:
+			self.keyblob = b''
+		data += len(self.saltblob).to_bytes(4, byteorder='little', signed=False)
+		data += len(self.keyblob).to_bytes(4, byteorder='little', signed=False)
+		data += self.saltblob
+		data += self.keyblob
+		return data
 
 	def get_key(self):
 		if self.keyblob[8:].startswith(b'RSA2'):
