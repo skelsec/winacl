e5dce915bf8d8ee32c8c791934e6e9e66a515fb3
SkelSec <info@skelsecprojects.com>
2023-02-15 21:12:42 +0100

    fixing whitespace errors

--- winacl/dtyp/security_descriptor.py
+++ winacl/dtyp/security_descriptor.py
@@ -139,9 +139,9 @@ class SECURITY_DESCRIPTOR:
 	def to_sddl(self, object_type = None):
 		t=''
 		if self.Owner is not None:
-        		t +=  'O:' + self.Owner.to_sddl()
-    		if self.Group is not None:
-        		t += 'G:' + self.Group.to_sddl()
+			t +=  'O:' + self.Owner.to_sddl()
+		if self.Group is not None:
+			t += 'G:' + self.Group.to_sddl()
 		if self.Sacl is not None:
 			t+= 'S:' + sddl_acl_control(self.Control) + self.Sacl.to_sddl(object_type)
 		if self.Dacl is not None:
